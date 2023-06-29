fa94d8eb686ee7595cba5c9f0601d2fb8e3fe592
Emmett Baker <ekbaker@proton.me>
2023-06-29 12:28:50 -0400

    mobile design part 2

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,16 +1,22 @@
+"use client";
+
 import { projects } from "@/data/projects";
 import Image from "next/image";
 import ProjectImage from "./ProjectImage";
 import ProjectDescription from "./ProjectDescription";
+import { useMediaQuery } from "@material-ui/core";
 
 function ProjectList() {
+
+  let isTabletOrPhone = useMediaQuery("(max-width:768px)");
+
   return (
     <div className="md:h-[calc(100vh-25vh)] overflow-auto">
       <ul>
         {projects.map((project, index) => (
           <li key={project.id} className="">
-            <div className={index % 2 === 1 ? 'grid grid-cols-2 bg-gray-300 py-10 px-5 text-black' : 'grid grid-cols-2 py-10 px-5'}>
-              {index % 2 === 1 ? (
+            <div className={index % 2 === 1 ? 'md:grid md:grid-cols-2 bg-gray-300 py-10 px-5 text-black' : 'md:grid md:grid-cols-2 py-10 px-5'}>
+              {index % 2 === 1 && !isTabletOrPhone ? (
                 <>
                   <ProjectDescription project={project} />
                   <div className="flex justify-end pl-5">
